d6e27ce515321fe14f526d4b3d5ed9e2f3824b33
Alexander Waya Nguku <alex.waya@gmail.com>
2025-02-23 17:21:07 +0300

    Stripe Hosted Page

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, success, cancel
 
 urlpatterns = [
-    path("/", home, name="home"),
+    path("", home, name="home"),
     path("success/", success, name="success"),
     path("cancel/", cancel, name="cancel"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  # new
+from django.conf import settings  # new
+from django.urls import reverse  # new
 
-# Create your views here.
+import stripe  # new
 
-def home(request):
+
+def home(request):  # new
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    if request.method == "POST":
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    "price": "price_1Qvfx9FiF6wRnuOduaw8v4cy",  # enter yours here!!!
+                    "quantity": 1,
+                }
+            ],
+            mode="subscription",
+            success_url=request.build_absolute_uri(reverse("success")),
+            cancel_url=request.build_absolute_uri(reverse("cancel")),
+        )
+        return redirect(checkout_session.url, code=303)
     return render(request, "products/home.html")
 
 
